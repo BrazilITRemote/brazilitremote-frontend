b1ffc42ec1abeda770af4910c1979c377adf7b91
Richard A <richard.arias@msn.com>
2025-04-26 14:23:18 -0300

    refactor: remove Head component from head.tsx

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Brazil IT Remote</title>
-      <meta name="description" content="Comunidade e workshops remotos de TI no Brasil" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </>
-  );
-}
