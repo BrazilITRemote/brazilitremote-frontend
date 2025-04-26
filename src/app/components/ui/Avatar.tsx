d2bb24c0577569b5554108c59ecca01d71aab11c
Richard A <richard.arias@msn.com>
2025-04-26 15:34:34 -0300

    feat: replace img tag with Image component in Avatar for improved performance

--- src/app/components/ui/Avatar.tsx
+++ src/app/components/ui/Avatar.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 export interface AvatarProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   size?: number;
   className?: string;
+  src: string;
 }
 
-export function Avatar({ size = 48, className = "", ...props }: AvatarProps) {
+export function Avatar({
+  size = 48,
+  className = "",
+  ...props
+}: Readonly<AvatarProps>) {
   return (
-    <img
+    <Image
       {...props}
+      src={props.src}
+      loading="lazy"
+      alt={props.alt ?? "Avatar"}
       width={size}
       height={size}
       className={`rounded-full object-cover ${className}`}
