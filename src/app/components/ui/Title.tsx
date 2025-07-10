21dfbbada315784696ae97cbdbed8ff4fb663397
thundree <129085761+thundree@users.noreply.github.com>
2025-07-09 22:57:57 -0300

    style: format JSX for better readability in PageTitle component

--- src/app/components/ui/Title.tsx
+++ src/app/components/ui/Title.tsx
@@ -9,9 +9,13 @@ export default function PageTitle({
 }: Readonly<PageTitleProps>) {
   return (
     <div className="mb-12 text-center">
-      <h1 className="text-4xl lg:text-5xl font-bold text-slate-800 dark:text-white mb-4">{title}</h1>
+      <h1 className="text-4xl lg:text-5xl font-bold text-slate-800 dark:text-white mb-4">
+        {title}
+      </h1>
       {description ? (
-        <p className="text-lg text-slate-600 dark:text-slate-300 max-w-2xl mx-auto">{description}</p>
+        <p className="text-lg text-slate-600 dark:text-slate-300 max-w-2xl mx-auto">
+          {description}
+        </p>
       ) : null}
     </div>
   );
