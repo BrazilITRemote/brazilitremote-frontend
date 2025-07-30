89f7d071c522f19526dec46ac272d027dcd70510
thundree <129085761+thundree@users.noreply.github.com>
2025-07-29 22:52:27 -0300

    fix: adjust layout and styling in OrganizersSection component for better responsiveness

--- src/app/components/OrganizersSection.tsx
+++ src/app/components/OrganizersSection.tsx
@@ -12,7 +12,7 @@ export default function OrganizersSection() {
       id="organizadores"
       className="py-20 lg:py-24 bg-white dark:bg-gray-900"
     >
-      <div className="container mx-auto px-4 text-center">
+      <div className="w-full md:container mx-auto px-2 md:px-4 text-center">
         <h2 className="text-3xl lg:text-4xl font-bold text-slate-800 dark:text-white">
           Nossos Organizadores
         </h2>
@@ -20,13 +20,13 @@ export default function OrganizersSection() {
           As pessoas apaixonadas que fazem a comunidade Brazil IT Remote
           acontecer.
         </p>
-        <div className="mt-12 flex justify-center gap-6 md:gap-8 flex-wrap">
+        <div className="w-full mt-12 px-0 flex justify-center gap-3 md:gap-8 flex-wrap">
           {activeOrganizers
             .toSorted((a, b) => a.name.localeCompare(b.name))
             .map((organizer) => (
               <div
                 key={organizer.id}
-                className="flex flex-col items-center text-center w-34 md:w-40"
+                className="flex flex-col items-center text-center w-[48%] md:w-40"
               >
                 <div className="w-16 h-16 md:w-20 md:h-20 rounded-full bg-slate-200 dark:bg-slate-700 ring-4 ring-white dark:ring-slate-700 shadow-lg flex items-center justify-center overflow-hidden">
                   {organizer.avatar ? (
@@ -44,7 +44,7 @@ export default function OrganizersSection() {
                 <h3 className="mt-4 text-xl font-bold text-slate-800 dark:text-white">
                   {organizer.name}
                 </h3>
-                <p className="flex flex-col text-slate-500 dark:text-slate-400 h-12">
+                <p className="flex flex-col text-slate-500 dark:text-slate-400 h-14">
                   {organizer.role.map((role, index) => (
                     <span key={`${organizer.id}-role-${index}`}>
                       {role}
